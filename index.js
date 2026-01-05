8c65f1bb8aaee211ff8f06218a9d1ceed77b7662
ighosh <inghosh30@gmail.com>
2026-01-06 01:15:55 +0530

    06-01

--- index.js
+++ index.js
@@ -18,4 +18,37 @@ const getSecondLargest = (a) => {
   return scLar;
 };
 
-console.log(getSecondLargest(ar));
+// console.log(getSecondLargest(ar));
+// is palindrome
+const isDummy = (number) => {
+  if (number < 0 || !Number.isInteger(number)) return false;
+  let rev = 0;
+  let original = number;
+  while (original != 0) {
+    let lastdigit = original % 10;
+    original = Math.floor(original / 10);
+    rev = rev * 10 + lastdigit;
+  }
+  console.log(rev);
+  return rev === number;
+};
+// console.log(isDummy(NaN));
+
+const removeDuplicates = (arr) => {
+  let s = 0;
+  let t = 0;
+  while (t < arr.length) {
+    if (arr[s] !== arr[t]) {
+      arr[++s] = arr[t];
+    }
+    t++;
+  }
+  return s + 1;
+};
+
+console.log(
+  removeDuplicates([
+    1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 5, 5, 5, 5,
+    5, 6,
+  ])
+);
