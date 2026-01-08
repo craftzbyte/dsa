a7f0c762bdefa632fa34e74b162491875a0ce1ac
ighosh <inghosh30@gmail.com>
2026-01-08 10:59:42 +0530

    7th jan

--- index.js
+++ index.js
@@ -46,9 +46,46 @@ const removeDuplicates = (arr) => {
   return s + 1;
 };
 
-console.log(
-  removeDuplicates([
-    1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 5, 5, 5, 5,
-    5, 6,
-  ])
-);
+let arrr = [0, 1, 2, 2, 3, 0, 4, 2];
+
+const removeElement = (nums, val) => {
+  let s = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[s++] = nums[i];
+    }
+  }
+  return [s, nums];
+};
+
+//reverse string array with o(1)} memory
+
+let a = ["a", "b", "c", "d"];
+const reverseString = (ar) => {
+  if (ar.length <= 1) return ar;
+  let s = 0;
+  let e = ar.length - 1;
+  let space;
+  while (e > s) {
+    space = ar[s];
+    ar[s] = ar[e];
+    ar[e] = space;
+    s++;
+    e--;
+  }
+  return ar;
+};
+
+const maxProfit = (arr = []) => {
+  let min = arr[0] || 0;
+  let maxProfit = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] - min > maxProfit) {
+      maxProfit = arr[i] - min;
+    }
+  }
+  return maxProfit;
+};
